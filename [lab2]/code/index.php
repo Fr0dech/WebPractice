fc16fb523704f037316065c3cd0ec8ed28af2379
Fr0dech <fr0dechsilpie@gmail.com>
2024-03-10 11:26:41 +0200

    Task 1 competed with your order
    If it's not what you ordered, then, well, sad to be you

--- [lab2]/code/index.php
+++ [lab2]/code/index.php
@@ -1,2 +1,11 @@
 <?php
-echo phpinfo();
+
+// Task 1
+/* Imagine a lot of code here*/
+
+$very_bad_unclear_name = "15 chicken wings";
+//Write your code here:
+$order = &$very_bad_unclear_name;
+$order .= ', a number nine large, a number six with extra dip...';
+//Dont change the line below
+echo "\nYour order is: $very_bad_unclear_name.";
