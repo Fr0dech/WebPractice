4c7ccdf88cceedeab67e304b4c3b570f4db9c284
Fr0dech <fr0dechsilpie@gmail.com>
2024-03-10 11:43:42 +0200

    Added Task 2 with echo and variables
    duh, what else can I say

--- [lab2]/code/index.php
+++ [lab2]/code/index.php
@@ -1,5 +1,5 @@
 <?php
-
+header('Content-type: text/plain');
 // Task 1
 /* Imagine a lot of code here*/
 
@@ -9,3 +9,20 @@ $order = &$very_bad_unclear_name;
 $order .= ', a number nine large, a number six with extra dip...';
 //Dont change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
+echo "\n";
+echo "\n";
+
+// Task 2
+$cumpot = 1;
+echo $cumpot;
+echo "\n";
+$sirgay = 1.3;
+echo $sirgay;
+echo "\n";
+echo 1337-1325;
+echo "\n";
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo $last_month - $this_month;
+echo "\n";
+echo "\n";
